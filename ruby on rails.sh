10c866303165afb7c18240a1c6ab62732d4572db
Arash <arashium@yahoo.com>
2014-07-17 22:09:01 +1000

    Update ruby on rails.sh

--- ruby on rails.sh
+++ ruby on rails.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # how to install ruby on rails for ubuntu
+
+# Install Ruby with RVM
 sudo apt-get update
 sudo apt-get install curl
 \curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm requirements
+
+# Install Ruby
 rvm install ruby
 rvm use ruby --default
+
+# Install RubyGems
 rvm rubygems current
+
+# Install Rails
 gem install rails
